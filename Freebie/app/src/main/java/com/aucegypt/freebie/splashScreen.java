0accdef737a473acdeb5b15f58266fd4ea50eff7
Ahmed Abouzaid <ahmedabouzaid@aucegypt.edu>
2021-01-16 19:20:58 +0200

    Splash + login and signup

--- Freebie/app/src/main/java/com/aucegypt/freebie/splashScreen.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/splashScreen.java
@@ -2,14 +2,17 @@ package com.aucegypt.freebie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Fade;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class splashScreen extends AppCompatActivity {
 
-    ImageView logo, splashImg;
+    ImageView logo;
     LottieAnimationView lottieAnimationView;
 
     @Override
@@ -18,12 +21,21 @@ public class splashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         logo = findViewById(R.id.logo1);
-        splashImg = findViewById(R.id.img);
         lottieAnimationView = findViewById(R.id.lottie1);
 
-        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
-        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
-        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1200).setDuration(1000).setStartDelay(4000);
+        logo.animate().rotationBy(360).setDuration(2000).setStartDelay(1000);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(splashScreen.this, afterSplash.class);
+                startActivity(intent);
+                splashScreen.this.finish();
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+
+            }
+        }, 3000);
 
 
 
