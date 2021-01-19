67593e1b48bd57607bb53be6eb493c6b04841d0e
Ahmed Abouzaid <ahmedabouzaid@aucegypt.edu>
2021-01-19 04:12:29 +0200

    Sign up with Firebase added

--- Freebie/app/src/main/java/com/aucegypt/freebie/MainActivity.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/MainActivity.java
@@ -4,23 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-Button btn1;
-Button noAccButton;
+    Button btn1;
+    Button noAccButton;
+    private EditText email, password;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn1=(Button)findViewById(R.id.signinbtn);
+        firebaseAuth = FirebaseAuth.getInstance();
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this, HomePage.class);
                 startActivity(intent);
+//                createUser();
             }
         });
 
@@ -33,6 +42,9 @@ Button noAccButton;
             }
         });
     }
+//    private void createUser(){
+//        String email =
+//    }
 }
 
 
--- Freebie/app/src/main/java/com/aucegypt/freebie/SignUp.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/SignUp.java
@@ -2,27 +2,48 @@ package com.aucegypt.freebie;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
+
 public class SignUp extends AppCompatActivity {
-Button btn1;
-TextView textView;
+    Button btn1;
+    TextView textView;
+    private EditText email, password, confirm_password;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         btn1=(Button)findViewById(R.id.signupbtn);
-        textView=(TextView)findViewById(R.id.signhvaccountid);
+        email=findViewById(R.id.signupemailtxt);
+        password = findViewById(R.id.passwordtxt);
+        confirm_password = findViewById(R.id.passwordtxt1);
+        firebaseAuth = FirebaseAuth.getInstance();
+        textView = findViewById(R.id.signhvaccountid);
+
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(SignUp.this,HomePage.class);
-                startActivity(intent);
+//                Intent intent=new Intent(SignUp.this,HomePage.class);
+//                startActivity(intent);
+                createUser();
             }
         });
         textView.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +53,50 @@ TextView textView;
                 startActivity(intent);
             }
         });
+    }
+    private void createUser() {
+        String mEmail = email.getText().toString();
+        String mPassword = password.getText().toString();
+        String mPassword1 = confirm_password.getText().toString();
+
+        if (!mEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+            if (!mPassword.isEmpty() && !mPassword1.isEmpty() && mPassword.equals(mPassword1) &&mPassword.length()>= 8) {
+                firebaseAuth.createUserWithEmailAndPassword(mEmail, mPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(SignUp.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignUp.this, MainActivity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(SignUp.this, "Your email is already registered.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUp.this, "Failed. Please try again!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else if(mPassword.length() < 8){
+                Toast.makeText(SignUp.this, "password must have at least 8 characters", Toast.LENGTH_SHORT).show();
+
+            }
+            else if(!mPassword.equals(mPassword1)) {
+                Toast.makeText(SignUp.this, "Password must match!", Toast.LENGTH_SHORT).show();
+            }
+            else if(mPassword.isEmpty()){
+                password.setError("Password cannot be empty");
+            }
+            else{
+                confirm_password.setError("Password cannot be empty");
+            }
+        } else if (mEmail.isEmpty()) {
+            email.setError("Email cannot be empty");
+        } else {
+            email.setError("Please enter a valid email");
+        }
     }
 }
