408993eb79c793c59f663ed610a346277b5e990e
Ahmed Abouzaid <ahmedabouzaid@aucegypt.edu>
2021-01-19 05:12:38 +0200

    Sign up and Login with Firebase added

--- Freebie/app/src/main/java/com/aucegypt/freebie/MainActivity.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/MainActivity.java
@@ -2,18 +2,25 @@ package com.aucegypt.freebie;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1;
-    Button noAccButton;
+    Button btn1, noAccButton;
     private EditText email, password;
     private FirebaseAuth firebaseAuth;
 
@@ -21,15 +28,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        email=findViewById(R.id.emailtxt);
+        password = findViewById(R.id.passwordtxt);
         btn1=(Button)findViewById(R.id.signinbtn);
         firebaseAuth = FirebaseAuth.getInstance();
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, HomePage.class);
-                startActivity(intent);
-//                createUser();
+//                Intent intent=new Intent(MainActivity.this, HomePage.class);
+//                startActivity(intent);
+                loginUser();
             }
         });
 
@@ -42,9 +50,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//    private void createUser(){
-//        String email =
-//    }
+    private void loginUser() {
+        String mEmail = email.getText().toString();
+        String mPassword = password.getText().toString();
+
+        if (!mEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+            if (!mPassword.isEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(mEmail, mPassword)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(MainActivity.this, "Logged in successfully!", Toast.LENGTH_SHORT).show();
+
+                                Intent intent=new Intent(MainActivity.this, HomePage.class);
+                                startActivity(intent);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            else{
+                password.setError("Password cannot be empty");
+            }
+        } else if (mEmail.isEmpty()) {
+            email.setError("Email cannot be empty");
+        } else {
+            email.setError("Please enter a valid email");
+        }
+    }
 }
 
 
