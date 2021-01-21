d4971892edd32c9110e332add0416835ea80515f
awael <awael@aucegypt.edu>
2021-01-21 20:09:01 +0200

    fixed fragments

--- Freebie/app/src/main/java/com/aucegypt/freebie/ngoView_private.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/ngoView_private.java
@@ -1,12 +1,14 @@
 package com.aucegypt.freebie;
 
-import android.app.Fragment;
 import android.os.Bundle;
 
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
--- Freebie/app/src/main/java/com/aucegypt/freebie/ngoView_public.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/ngoView_public.java
@@ -1,11 +1,12 @@
 package com.aucegypt.freebie;
 
-import android.app.Fragment;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
