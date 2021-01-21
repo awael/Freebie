413d3c1111ddfa7add923cf291c1b8c2f421cc5b
awael <awael@aucegypt.edu>
2021-01-21 21:00:43 +0200

    donations backend

--- Freebie/app/src/main/java/com/aucegypt/freebie/ItemActivity.java
+++ Freebie/app/src/main/java/com/aucegypt/freebie/ItemActivity.java
@@ -18,6 +18,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -26,11 +28,22 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ItemActivity extends AppCompatActivity {
     private static final String TAG = "retrieve database item";
     Button donate;
     Button editAddress;
-    EditText addressHint;
+    EditText addressHint, notesText;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    DocumentSnapshot document;
+    String cat;
+    Spinner spinner1;
+    String userID;
+
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
@@ -65,9 +78,11 @@ public class ItemActivity extends AppCompatActivity {
         switch (widgetNum){
             case 0:
                 uri = "@drawable/clothes";
+                cat = "Clothes";
                 break;
             case 1:
                 uri = "@drawable/foodicon";
+                cat = "Food";
                 ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(ItemActivity.this,
                         android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.foodngos));
                 myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -75,42 +90,67 @@ public class ItemActivity extends AppCompatActivity {
                 break;
             case 2:
                 uri = "@drawable/blankets";
+                cat = "Blankets";
+
                 break;
             case 3:
                 uri = "@drawable/firstaid";
+                cat = "First-Aid";
                 break;
             case 4:
                 uri = "@drawable/furniture";
+                cat = "Furniture";
+
                 break;
             case 5:
                 uri = "@drawable/gadgets";
+                cat = "Gadgets";
+
                 break;
             case 6:
                 uri = "@drawable/medicine";
+                cat = "Medicine";
+
                 break;
             case 7:
                 uri = "@drawable/bags";
+                cat = "Bags";
+
                 break;
             case 8:
                 uri = "@drawable/sanitary";
+                cat = "Sanitary";
+
                 break;
             case 9:
                 uri = "@drawable/supplies";
+                cat = "Supplies";
+
                 break;
             case 10:
                 uri = "@drawable/toys";
+                cat = "Toys";
+
                 break;
             case 11:
                 uri = "@drawable/utensils";
+                cat = "Utensils";
+
                 break;
             case 12:
                 uri = "@drawable/sports";
+                cat = "Sports";
+
                 break;
             case 13:
                 uri = "@drawable/others";
+                cat = "Others";
+
                 break;
             default:
                 uri = "@drawable/others";
+                cat = "Others";
+
         }
         int imageResource = getResources().getIdentifier(uri, null, getPackageName());
         ImageView imageview = (ImageView) findViewById(R.id.img_logo);
@@ -122,13 +162,14 @@ public class ItemActivity extends AppCompatActivity {
         relativeLayout = findViewById(R.id.rel1);
         handler.postDelayed(runnable, 500);
 
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        DocumentReference docRef = db.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        DocumentReference docRef = db.collection("Users").document(userID);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
+                    document = task.getResult();
                     if (document.exists()) {
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                         EditText addressHint = (EditText)findViewById(R.id.addressHint);
@@ -155,13 +196,48 @@ public class ItemActivity extends AppCompatActivity {
             }
         });
 
+        spinner1=(Spinner) findViewById(R.id.spinner1);
+        notesText = (EditText)findViewById(R.id.notesText);
+
+
         donate=(Button)findViewById(R.id.donateButton);
         donate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ItemActivity.this,"Donation successful, Thank You!",Toast.LENGTH_LONG).show();
-                Intent intent=new Intent(ItemActivity.this, HomePage.class);
-                startActivity(intent);
+
+                Map<String, Object> donation = new HashMap<>();
+//                String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                donation.put("userID", userID);
+                donation.put("category", cat );
+                donation.put("address", document.getString("address"));
+                donation.put("email", document.getString("email"));
+                donation.put("fName", document.getString("fName"));
+                donation.put("phone", document.getString("phone"));
+                donation.put("ngo", spinner1.getSelectedItem().toString());
+                donation.put("notes", notesText.getText().toString());
+
+
+                db.collection("Donations")
+                        .add(donation)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                Toast.makeText(ItemActivity.this,"Donation successful, Thank You!",Toast.LENGTH_LONG).show();
+                                Intent intent=new Intent(ItemActivity.this, HomePage.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+
+
+
             }
         });
 
